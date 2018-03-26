40918428ce7f69bf964b3b9c042598f53006f813
Diaonic <diaonic@gmail.com>
2018-03-26 15:12:24 -0400

    Second Commit

--- iot_print_server.py
+++ iot_print_server.py
@@ -1,79 +1,68 @@
-#!/usr/bin/python
-#import flask and printer libs
+#!flask/bin/python
+
 from flask import Flask, request
 from escpos.printer import Usb
 
-#Vars
 p = Usb(0x0416, 0x5011)
 app = Flask(__name__)
-#index
+
+#CREATE 'INDEX' PAGE
 @app.route('/')
 def index():
-        return 'Flask server running!'
+	return 'Your Flask server is working!'
 
-#list
+#CREATE 'LIST' PAGE FOR PRINTING SHOPPING LIST
 @app.route('/list')
 def list():
-        #capture data from web hooks
-        content = request.get_data()
-        #convert raw data to string
-        str_content = str(content)
-        #split data into seperate lines
-        str_split = str_content.splitlines()
-        #seperate words by comma
-        newlist = []
-        for word in str_split:
-                word = word.split(',')
-                newlist.extend(word)
-        #cleanup formatting
-        rmv_marks = [s.strip("b'") for s in newlist]
-        #print
-        p.text("Shopping List:\n")
-        #iterate over list
-        r = 1
-        for x in rmv_marks:
-                p.text(str(r) + ". " + x + "\n")
-                r += 1
-        return x
-#runa
-if __name__ == '__main__':
-        app.run(host='0.0.0.0')
-#import flask and printer libs
-from flask import Flask, request
-from escpos.printer import Usb
 
-#Vars
-p = Usb(0x0416, 0x5011)
-app = Flask(__name__)
-#index
-@app.route('/')
-def index():
-        return 'Flask server running!'
+	#REQUEST DATA FROM WEBHOOKS
+	content = request.get_data()
 
-#list
-@app.route('/list')
+	#CONVERT RAW DATA TO STRING
+	str_content = str(content)
+
+	#DIVIDE DATA INTO SEPERATE LINES
+	str_split = str_content.splitlines()
+
+	#SEPERATE WORDS BY COMMA AND ADD TO A NEW LIST
+	newlist = []
+	for word in str_split:
+		word = word.split(',')
+		newlist.extend(word)
+
+	#REMOVE FORMATTING MARKS
+	rmv_marks = [s.strip("b'") for s in newlist]
+
+	#PRINT HEADER
+	#print("Shopping List\n")
+	p.text("Shopping List:\n")
+
+	#ENUMERATE AND PRINT EACH ITEM IN LIST
+	r = 1
+	for x in rmv_marks:
+		#print(str(r) + ". " + x + "\n")
+		p.text(str(r) + ". " + x + "\n")
+		r += 1
+
+	return 'x'
+
+#CREATE 'TO DO' PAGE FOR PRINTING TO DO LIST
+@app.route('/todo')
 def list():
-        #capture data from web hooks
-        content = request.get_data()
-        #convert raw data to string
-        str_content = str(content)
-        #split data into seperate lines
-        str_split = str_content.splitlines()
-        #seperate words by comma
-        newlist = []
-        for word in str_split:
-                word = word.split(',')
-                newlist.extend(word)
-        #cleanup formatting
-        rmv_marks = [s.strip("b'") for s in newlist]
-        #print
-        p.text("Shopping List:\n")
-        #iterate over list
-        r = 1
-        for x in rmv_marks:
-                p.text(str(r) + ". " + x + "\n")
-                r += 1
-        return x
-#run
+	content = request.get_data()
+	str_content = str(content)
+	str_split = str_content.splitlines()
+	newlist = []
+	for word in str_split:
+		word = word.split(',')
+		newlist.extend(word)
+	rmv_marks = [s.strip("b'") for s in newlist]
+	p.text("To Do List:\n")
+	r = 1
+	for x in rmv_marks:
+		p.text(str(r) + ". " + x + "\n")
+		r += 1
+	return 'x'
+
 if __name__ == '__main__':
-        app.run(host='0.0.0.0')
+	app.run(debug=True, host='0.0.0.0')
